b68dba931c1707cbaf2e2da82a59f4532822e749
ZSHU <zshu@ets.org>
2015-07-13 12:22:47 -0400

    update the format

--- Tree_0630.py
+++ Tree_0630.py
@@ -14,15 +14,7 @@ class Treenode:
         self.right=None 
         
  
-root1=Treenode(3)
-root2=Treenode(9)
-root3=Treenode(20)
-root4=Treenode(15)
-root5=Treenode(7)
-root1.left=root2
-root1.right=root3
-root3.left=root4
-root3.right=root5
+
        
 class Solution: 
     """ 
@@ -32,6 +24,11 @@ class Solution:
     """
     
     def numTrees(self, n):
+        """
+        purpose: this function is to compute the number of all possible trees based on the catalan number
+        n: the input, the number of nodes in the binary tree 
+        by: zhan shu, shuzhancnjx@gmail.com         
+        """
         dp=[1,1,2]
         if n<=2: return dp[n]
         else:
@@ -42,6 +39,11 @@ class Solution:
             return dp[n]
 
     def generateTrees(self, n):
+        """
+        purpose: this is a function to generate all possible binary trees based on n nodes
+        n: the number of nodes 
+        by: zhan shu, shuzhancnjx@gmail.com
+        """
         nums=range(1,n+1)
         def generate(nums):
             if len(nums)==0: return [None]
@@ -61,7 +63,10 @@ class Solution:
     
     def inorder_recursive(self, root, res):
         """
-           this is a recursive solution        
+           purpose: this function is to perform inorder tranversial 
+           root: the binary tree 
+           res: the final result, start with []
+           by: zhan shu, shuzhancnjx@gmail.com
         """
         if root==None:
             return 
@@ -72,7 +77,14 @@ class Solution:
             return res
             
     def preorder_recursive(self, root, res):
-        if root==None: return 
+        """
+           purpose: this function is to perform perorder tranversial 
+           root: the binary tree 
+           res: the final result, start with []
+           by: zhan shu, shuzhancnjx@gmail.com
+        """
+        if root==None:
+            return 
         else:
             res+=[root.val]
             self.preorder_recursive(root.left, res)
@@ -80,6 +92,12 @@ class Solution:
             return res
             
     def postorder_recursive(self, root, res):
+        """
+           purpose: this function is to perform postorder tranversial 
+           root: the binary tree 
+           res: the final result, start with []
+           by: zhan shu, shuzhancnjx@gmail.com
+         """
         if root==None: return 
         else:
             self.postorder_recursive(root.left,res)
@@ -88,19 +106,226 @@ class Solution:
             return res
             
     def validTree(self, root):
+        """
+           purpose: this function is to verify a binary 
+           root: the binary tree 
+           by: zhan shu, shuzhancnjx@gmail.com
+        """
         res=self.inorder_recursive(root,[])
         return res==sorted(res)
         
+    def invert(self, root):
+        """
+           purpose: this function is to invert a binary tree 
+           root: the binary tree 
+           by: zhan shu, shuzhancnjx@gmail.com
+         """
+        if root.left==None and root.right==None:
+            return root
+        else: 
+            left=self.invert(root.left)
+            root.left=self.invert(root.right)
+            root.right=left 
+            return root    
+
+    def pathTree(self, root):
+        """
+           purpose: this function is to return all possible pathes from the top to the deaf 
+           root: the binary tree 
+           by: zhan shu, shuzhancnjx@gmail.com
+         """
+        def path(root,pa):
+            if root.left==None and root.right==None:
+                res.append(pa[:])
+                return 
+            else:
+                if root.left!=None:
+                    path(root.left,pa+[root.left.val])
+                if root.right!=None:
+                    path(root.right,pa+[root.right.val])
+        res=[]
+        path(root, [root.val])
+        return res
+
+    def buildInPost(self, inorder, postorder):
+        """
+           purpose: this function is to build a binary tree based its inorder and postorder tranversial
+           inorder: the inorder transverial result, a list 
+           postorder: the postorder transverial result, a list 
+           by: zhan shu, shuzhancnjx@gmail.com
+        """
+        if len(inorder)==0:
+            return None
+        else:
+            root=Treenode(postorder[-1])
         
+            i=0
+            while i<len(inorder):
+                if inorder[i]!=postorder[-1]:
+                    i+=1
+                else:
+                    break   
+            root.left=self.buildInPost(inorder[:i], postorder[:i])
+            root.right=self.buildInPost(inorder[i+1:], postorder[i:(len(postorder)-1)])
+            return root
+
+    def isSystematic(self, root):
+        """
+           purpose: this function is to verify if a binary tree is systematic 
+           root: the binary tree 
+           by: zhan shu, shuzhancnjx@gmail.com
+        """
+        def sys(left, right):
+            if left==None and right==None: 
+                return True
+            elif left==None or right==None:
+                return False
+            elif left.val!=right.val:
+                return False
+            else:
+                return  sys(left.left, right.left) and  sys(left.right, right.right)
+    
+        if root==None: 
+            return True 
+        else:
+            return sys(root.left, root.right)
+           
+
+    def sameTree(self, p, q):
+        """
+           purpose: this function is to test if two binary trees equal to each other 
+           p: a binary tree 
+           q: the other binary tree 
+           by: zhan shu, shuzhancnjx@gmail.com
+        """
+        if p==None and q==None: 
+            return True 
+        elif p==None or q==None:
+            return False 
+        elif p.val!=q.val:
+            return False 
+        else:
+            return self.sameTree(p.left, q.left) and self.sameTree(p.right, q.right)
+
+    def upsideDown(self, root):
+        """
+           purpose: this function is to upside down a binary tree with the constrains posted by Leetcode 
+           root: the binary tree 
+           by: zhan shu, shuzhancnjx@gmail.com
+        """
+        stack=[]
+        while root.left:
+            stack.append(root)
+            root=root.left
+        p=root 
+        while stack:
+            parent=stack.pop()
+            root.left=parent.right
+            root.right=parent 
+            root=root.right
+        root.left, root.right=None, None 
+        return p
+
+    def zzLevel(self, root):
+        """
+           purpose: this function is to perform a level-tranversial with zigzag outputs  
+           root: the binary tree 
+           by: zhan shu, shuzhancnjx@gmail.com
+        """
+        def zigzag(root, level, res):
+            if root:
+                if level>len(res)-1:
+                    res+= [[] for i in range( level-len(res)+1)]
+                if level%2==0: 
+                    res[level].append(root.val)
+                if level%2==1:
+                    res[level].insert(0, root.val)
+                zigzag(root.left, level+1, res)
+                zigzag(root.right, level+1, res)
+    
+        level=0
+        res=[[]]
+        zigzag(root, level, res)
+        return res
+    
+    def maxSum(self, root):
+        """
+           purpose: this function is to find out the maximum sum of any subtree  
+           root: the binary tree 
+           by: zhan shu, shuzhancnjx@gmail.com
+        """
+        def maxPath(root,ma):
+            if root.left==None and root.right==None:
+                return root.val
+            else:
+                temp=root.val; lmax=0;rmax=0
+                if root.left!=None:
+                    lmax=maxPath(root.left,ma)
+                    if lmax>0:
+                        temp+=lmax
+                if root.right!=None:
+                    rmax=maxPath(root.right,ma)
+                    if rmax>0:
+                        temp+=rmax
+                if temp>ma: ma=temp
+                return max(max(root.val+lmax, root.val+rmax), root.val)
+        
+        if root==None: 
+            return 0
+        else:
+            return maxPath(root,-1000000)
+             
+            
+    def Depth(self, root):
+        """
+        purpose: this function is to return maximum and minimum depths, recursive functions
+        root: the binary tree 
+        by: zhan shu, shuzhancnjx@gmail.com
+        """
+        def mmdepth(root, depth):
+            global maxdepth, mindepth
+            if root==None: 
+                if depth>maxdepth: maxdepth=depth
+                if depth<mindepth: mindepth=depth
+                return
+            else:
+                mmdepth(root.left, depth+1)
+                mmdepth(root.right, depth+1) 
+                return [maxdepth, mindepth]
+        maxdepth, mindepth=1,100000
+        res=mmdepth(root, 0)
+        return res
+        
+        
+
+"""
+simple test cases 
+"""
+root1=Treenode(3)
+root2=Treenode(9)
+root3=Treenode(20)
+root4=Treenode(15)
+root5=Treenode(7)
+root1.left=root2
+root1.right=root3
+root3.left=root4
+root3.right=root5
+
 
 Solution().numTrees(3)
 Solution().generateTrees(3)
 Solution().inorder_recursive(root1,[])
 Solution().preorder_recursive(root1,[])
 Solution().postorder_recursive(root1,[])
+Solution().Depth(root1)
+Solution().maxSum(root1)
+Solution().zzLevel(root1)
+Solution().upsideDown(root1)
+Solution().sameTree(root1,root1)
+Solution().isSystematic(root1)
 
 """
- the next three are interative-based inorder, preorder and postorder transveral 
+ the next are interative-based inorder, preorder and postorder transveral 
 """
 
 def inorderIter(root):
@@ -154,202 +379,6 @@ def postorder(root):
                 break
     return postorder
 
-"""
- a sysmetic tree
-"""
-
-root1=Treenode(1)
-root2=Treenode(2)
-root3=Treenode(2)
-root4=Treenode(3)
-root5=Treenode(3)
-root6=Treenode(4)
-root7=Treenode(4)
-root8=Treenode(5)
-root1.left=root2
-root1.right=root3
-root2.left=root4
-root2.right=root6
-root3.left=root5
-root3.right=root7
-
-
-def isSystematic(root):
-    
-    def sys(left, right):
-        if left==None and right==None: 
-            return True
-        elif left==None or right==None:
-            return False
-        elif left.val!=right.val:
-            return False
-        else:
-            return  sys(left.left, right.left) and  sys(left.right, right.right)
-    
-    if root==None: 
-        return True 
-    else:
-        return sys(root.left, root.right)
-           
-
-def sameTree(p, q):
-    if p==None and q==None: 
-        return True 
-    elif p==None or q==None:
-        return False 
-    elif p.val!=q.val:
-        return False 
-    else:
-        return sameTree(p.left, q.left) and sameTree(p.right, q.right)
-               
-"""
-a sample tree for zigzag level ordering 
-"""
-root1=Treenode(1)
-root2=Treenode(2)
-root3=Treenode(3)
-root4=Treenode(4)
-root5=Treenode(5)
-root6=Treenode(6)
-root7=Treenode(7)
-root8=Treenode(8)
-root9=Treenode(9)
-root1.left=root2
-root1.right=root3
-root2.left=root4
-root2.right=root5
-root3.left=root6
-root3.right=root7
-root6.left=root8
-root6.right=root9
-root4.left=Treenode(0)
-
-def zzLevel(root):
-    
-    def zigzag(root, level, res):
-        if root:
-            if level>len(res)-1:
-                res+= [[] for i in range( level-len(res)+1)]
-            if level%2==0: 
-                res[level].append(root.val)
-            if level%2==1:
-                res[level].insert(0, root.val)
-            zigzag(root.left, level+1, res)
-            zigzag(root.right, level+1, res)
-    
-    level=0
-    res=[[]]
-    zigzag(root, level, res)
-    return res 
-"""
-in the recursive functions, how to save the results at each step is really important, 
-1) the data type in return, single number, list, or other 
-2) using global variable or local variable to save the results of each step 
-
-the zigzag and pathTree are two typical examples. 
-"""         
-
-def pathTree(root):
-    
-    def path(root,pa):
-        if root.left==None and root.right==None:
-            res.append(pa[:])
-            return 
-        else:
-            if root.left!=None:
-"""
-note: the variable pa only exists in the path function, not accumulated across left and right tree
-"""
-                path(root.left,pa+[root.left.val])
-            if root.right!=None:
-                path(root.right,pa+[root.right.val])
-    res=[]
-    path(root, [root.val])
-    return res 
-
-root1=Treenode(1)
-root2=Treenode(2)
-root3=Treenode(3)
-root1.left=root2
-root1.right=root3
-
-"""
-maximum path, thanks the post from internet. 
-"""
-
-def maxSum(root):
-    def maxPath(root,ma):
-        if root.left==None and root.right==None:
-            return root.val
-        else:
-            temp=root.val; lmax=0;rmax=0
-            if root.left!=None:
-                lmax=maxPath(root.left,ma)
-                if lmax>0:
-                    temp+=lmax
-            if root.right!=None:
-                rmax=maxPath(root.right,ma)
-                if rmax>0:
-                    temp+=rmax
-            if temp>ma: ma=temp
-            return max(max(root.val+lmax, root.val+rmax), root.val)
-        
-    if root==None: 
-        return 0
-    else:
-        maxPath(root,-1000000)
-    return ma
- 
-"""
-maximum and minimum depths, recursive functions
-"""   
-def Depth(root):
-    def mmdepth(root, depth):
-        global maxdepth, mindepthdf
-        if root==None: 
-            if depth>maxdepth: maxdepth=depth
-            if depth<mindepth: mindepth=depth
-            return
-        else: 
-            mmdepth(root.left, depth+1)
-            mmdepth(root.right, depth+1)
-            return [maxdepth,mindepth]
-    maxdepth=-1
-    mindepth=100**100  
-    return mmdepth(root, 0)
-
-"""
-upside down, three methods, iteration, recursive, and traversial 
-"""
-root1=Treenode(1)
-root2=Treenode(2)
-root3=Treenode(3)
-root4=Treenode(4)
-root5=Treenode(5)
-root1.left=root2
-root1.right=root3
-root2.left=root4
-root2.right=root5
-
-
-root=root1
-
-def upDownIII(root):
-    
-    stack=[]
-    while root.left:
-        stack.append(root)
-        root=root.left
-    p=root 
-    
-    while stack:
-        parent=stack.pop()
-        root.left=parent.right
-        root.right=parent 
-        root=root.right
-    root.left, root.right=None, None 
-    
-    return p
 
 def upsideDownI(root):
     p, parent, parent_right=root, None, None 
@@ -380,32 +409,6 @@ def upsideDownII(root):
         
     return upside(root1, None)
 
-
-def buildInPost(inorder, postorder):
-    
-    if len(inorder)==0:
-        return None
-    else:
-        root=Treenode(postorder[-1])
-        
-        i=0
-        while i<len(inorder):
-            if inorder[i]!=postorder[-1]:
-                i+=1
-            else:
-                break
-            
-        root.left=buildInPost(inorder[:i], postorder[:i])
-        root.right=buildInPost(inorder[i+1:], postorder[i:(len(postorder)-1)])
-        
-        return root
-    
-"""
-invert binary tree by iteration and recursive functions 
-by zhan shu, shuzhancnjx@gmail.com
-
-"""
-
 def invertBinaryTree(root):
     
     stack=[]
@@ -424,26 +427,7 @@ def invertBinaryTree(root):
 
 
 
-           
-root1=Treenode(1)
-root2=Treenode(2)
-root3=Treenode(3)
-root4=Treenode(4)
-root5=Treenode(5)
-root1.left=root2
-root1.right=root3
-root2.left=root4
-root2.right=root5            
-         
-def invert(root):
-    if root.left==None and root.right==None:
-        return root
-    else: 
-        left=invert(root.left)
-        root.left=invert(root.right)
-        root.right=left 
-        return root
-        
+
         
           
             
