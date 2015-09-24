849584ab4fa1a1c722d16fcfec672ce070ad95b4
Zhan <shuzhancnjx@gmail.com>
2015-09-24 18:40:16 -0400

    add a more simplified version

--- Compare Version Numbers.py
+++ Compare Version Numbers.py
@@ -40,3 +40,34 @@ class Solution(object):
             if int(version1[len(version1)-1])>int(version2[len(version1)-1]): return 1
             elif int(version1[len(version1)-1])<int(version2[len(version1)-1]): return -1 
             else: return 0 
+            
+"""
+the same logic as above, but a more simplified version 
+"""
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        version1=version1.split('.'); version2=version2.split('.')
+        l1=len(version1);l2=len(version2); i=0
+        while i<l1 and i<l2:
+            if int(version1[i])>int(version2[i]):
+                return 1
+            elif int(version1[i])<int(version2[i]):
+                return -1
+            i+=1
+        
+        if l1>l2:
+            for letter in version1[i:]:
+                if int(letter)>0:
+                    return 1
+      
+        elif l1<l2:
+            for letter in version2[i:]:
+                if int(letter)>0:
+                    return -1
+        return 0
+       
